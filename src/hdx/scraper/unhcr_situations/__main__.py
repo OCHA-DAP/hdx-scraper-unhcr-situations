9f9408d139d1defbf94346fcfff680fc7407419c
Briar <briar.j.mills@gmail.com>
2025-06-17 11:27:04 -0400

    Refactor to latest setup

---
+++ src/hdx/scraper/unhcr_situations/__main__.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+"""
+Top level script. Calls other functions that generate datasets that this
+script then creates in HDX.
+
+"""
+
+import logging
+from os.path import dirname, expanduser, join
+
+from hdx.api.configuration import Configuration
+from hdx.data.hdxobject import HDXError
+from hdx.data.user import User
+from hdx.facades.infer_arguments import facade
+from hdx.utilities.downloader import Download
+from hdx.utilities.errors_onexit import ErrorsOnExit
+from hdx.utilities.path import temp_dir
+from hdx.utilities.retriever import Retrieve
+
+from unhcr_situations import UNHCRSituations
+
+logger = logging.getLogger(__name__)
+
+_USER_AGENT_LOOKUP = "hdx-scraper-unhcr-situations"
+_SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
+_UPDATED_BY_SCRIPT = "HDX Scraper: UNHCR Situations"
+
+
+def main(save: bool = False, use_saved: bool = False) -> None:
+    """Generate datasets and create them in HDX
+
+    Args:
+        save (bool): Save downloaded data. Defaults to True.
+        use_saved (bool): Use saved data. Defaults to False.
+
+    Returns:
+        None
+    """
+    configuration = Configuration.read()
+    User.check_current_user_write_access("unhcr")
+    with ErrorsOnExit() as errors:
+        with temp_dir(_USER_AGENT_LOOKUP) as temp_folder:
+            with Download() as downloader:
+                retriever = Retrieve(
+                    downloader, temp_folder, "saved_data", temp_folder, save, use_saved
+                )
+                unhcr_situations = UNHCRSituations(
+                    configuration, retriever, temp_folder, errors
+                )
+                unhcr_situations.get_data_from_hdx(configuration["dataset_name"])
+                unhcr_situations.get_data_from_unhcr()
+                dataset = unhcr_situations.generate_dataset()
+                if dataset:
+                    dataset.update_from_yaml(
+                        path=join(
+                            dirname(__file__), "config", "hdx_dataset_static.yaml"
+                        )
+                    )
+                    dataset["notes"] = dataset["notes"].replace(
+                        "\n", "  \n"
+                    )  # ensure markdown has line breaks
+                    try:
+                        dataset.create_in_hdx(
+                            remove_additional_resources=True,
+                            match_resource_order=False,
+                            hxl_update=False,
+                            updated_by_script=_UPDATED_BY_SCRIPT,
+                        )
+                    except HDXError:
+                        errors.add("Could not upload dataset to HDX")
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
+        user_agent_lookup=_USER_AGENT_LOOKUP,
+        project_config_yaml=join(
+            dirname(__file__), "config", "project_configuration.yaml"
+        ),
+    )
--- tests/test_unhcr_situations.py
+++ tests/test_unhcr_situations.py
@@ -3,20 +3,16 @@
 Unit tests for unhcr situations.
 
 """
+
 from os.path import join
 
-import pytest
-from hdx.api.configuration import Configuration
-from hdx.data.dataset import Dataset
-from hdx.data.vocabulary import Vocabulary
 from hdx.utilities.compare import assert_files_same
 from hdx.utilities.downloader import Download
 from hdx.utilities.errors_onexit import ErrorsOnExit
 from hdx.utilities.path import temp_dir
 from hdx.utilities.retriever import Retrieve
-from hdx.utilities.useragent import UserAgent
 
-from unhcr_situations import UNHCRSituations
+from hdx.scraper.unhcr_situations.unhcr_situations import UNHCRSituations
 
 
 class TestUNHCRSituations:
@@ -25,10 +21,23 @@ class TestUNHCRSituations:
         "title": "UNHCR Situations: Monthly Refugees and Asylum Seekers",
         "maintainer": "ac47b0c8-548b-4c37-a685-7377e75aad55",
         "owner_org": "abf4ca86-8e69-40b1-92f7-71509992be88",
-        "groups": [{"name": "ben"}, {"name": "civ"}, {"name": "gin"}, {"name": "sdn"}, {"name": "ssd"}, {"name": "uga"}],
+        "groups": [
+            {"name": "ben"},
+            {"name": "civ"},
+            {"name": "gin"},
+            {"name": "sdn"},
+            {"name": "ssd"},
+            {"name": "uga"},
+        ],
         "tags": [
-            {"name": "asylum seekers", "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1"},
-            {"name": "refugees", "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1"},
+            {
+                "name": "asylum seekers",
+                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+            },
+            {
+                "name": "refugees",
+                "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+            },
         ],
         "dataset_date": "[2023-05-31T00:00:00 TO 2024-06-30T23:59:59]",
         "package_creator": "briar-mills",
@@ -51,53 +60,57 @@ class TestUNHCRSituations:
         "url_type": "upload",
     }
 
-    @pytest.fixture(scope="function")
-    def fixtures(self):
-        return join("tests", "fixtures")
-
-    @pytest.fixture(scope="function")
-    def configuration(self):
-        Configuration._create(
-            hdx_read_only=True,
-            user_agent="test",
-            project_config_yaml=join("config", "project_configuration.yaml"),
-        )
-        UserAgent.set_global("test")
-        tags = (
-            "asylum seekers",
-            "refugees",
-        )
-        Vocabulary._tags_dict = {tag: {"Action to Take": "ok"} for tag in tags}
-        tags = [{"name": tag} for tag in tags]
-        Vocabulary._approved_vocabulary = {
-            "tags": tags,
-            "id": "b891512e-9516-4bf5-962a-7a289772a2a1",
-            "name": "approved",
-        }
-        return Configuration.read()
-
-    def test_main(self, configuration, fixtures):
-        with temp_dir(
-                "test_unhcr_situations", delete_on_success=True, delete_on_failure=False
-        ) as folder:
+    def test_main(
+        self,
+        configuration,
+        read_dataset,
+        fixtures_dir,
+        input_dir,
+        config_dir,
+    ):
+        with temp_dir("test_unhcr_situations") as tempdir:
             with Download() as downloader:
-                retriever = Retrieve(downloader, folder, fixtures, folder, False, True)
-                unhcr_situations = UNHCRSituations(configuration, retriever, folder, ErrorsOnExit())
-                dataset = Dataset.load_from_json(join(fixtures, f"{configuration['dataset_name']}.json"))
-                unhcr_situations.get_data_from_hdx(dataset)
+                retriever = Retrieve(
+                    downloader=downloader,
+                    fallback_dir=tempdir,
+                    saved_dir=input_dir,
+                    temp_dir=tempdir,
+                    save=False,
+                    use_saved=True,
+                )
+                unhcr_situations = UNHCRSituations(
+                    configuration, retriever, tempdir, ErrorsOnExit()
+                )
+                unhcr_situations.get_data_from_hdx(configuration["dataset_name"])
                 assert len(unhcr_situations.old_data) == 4
                 assert unhcr_situations.old_data[0] == {
-                    "Country": "Benin", "ISO3": "BEN", "Country of Origin": "Togo", "ISO3 of Origin": "TGO", "Population type": "Refugees", "Source": "UNHCR, Government", "Date": "2024-05-31", "Individuals": "4716"
+                    "Country": "Benin",
+                    "ISO3": "BEN",
+                    "Country of Origin": "Togo",
+                    "ISO3 of Origin": "TGO",
+                    "Population type": "Refugees",
+                    "Source": "UNHCR, Government",
+                    "Date": "2024-05-31",
+                    "Individuals": "4716",
                 }
                 unhcr_situations.get_data_from_unhcr(geo_ids=["220", "259", "295"])
                 assert len(unhcr_situations.new_data) == 21
                 assert unhcr_situations.new_data[0] == {
-                    "Country": "Uganda", "ISO3": "UGA", "Country of Origin": "South Sudan", "ISO3 of Origin": "SSD", "Population type": "Refugees", "Source": "Office of the Prime Minister, UNHCR, Government", "Date": "2024-06-30", "Individuals": "948191"
+                    "Country": "Uganda",
+                    "ISO3": "UGA",
+                    "Country of Origin": "South Sudan",
+                    "ISO3 of Origin": "SSD",
+                    "Population type": "Refugees",
+                    "Source": "Office of the Prime Minister, UNHCR, Government",
+                    "Date": "2024-06-30",
+                    "Individuals": "948191",
                 }
                 dataset = unhcr_situations.generate_dataset()
-                dataset.update_from_yaml()
+                dataset.update_from_yaml(
+                    path=join(config_dir, "hdx_dataset_static.yaml")
+                )
                 assert dataset == self.dataset
                 resources = dataset.get_resources()
                 assert resources[0] == self.resource
                 file = f"{configuration['dataset_name']}.csv"
-                assert_files_same(join(fixtures, file), join(folder, file))
+                assert_files_same(join(fixtures_dir, file), join(tempdir, file))
